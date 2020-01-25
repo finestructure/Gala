1204c73506582dd1a11c5abb1fce4ab368584ce6
Sven A. Schmidt <sas@finestructure.co>
2020-01-25 10:57:33 +0100

    Make public

--- Sources/PreviewProviderTools/PreviewProviderTools.swift
+++ Sources/PreviewProviderTools/PreviewProviderTools.swift
@@ -2,7 +2,7 @@ import SwiftUI
 
 
 @available(OSX 10.15.0, *)
-func NightAndDay<A: View>(_ name: String? = nil, @ViewBuilder items: @escaping () -> A) -> some View {
+public func NightAndDay<A: View>(_ name: String? = nil, @ViewBuilder items: @escaping () -> A) -> some View {
     ForEach([ColorScheme.light, .dark], id: \.self) { scheme in
         items()
             .previewDisplayName(name.map { "\($0) \(scheme)" } ?? "\(scheme)")
