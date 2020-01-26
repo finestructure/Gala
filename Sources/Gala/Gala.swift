e72ceb347f567e898bde4456aa391b9a84df2ce1
Sven A. Schmidt <sas@finestructure.co>
2020-01-26 10:47:26 +0100

    Use allCases where applicable

--- Sources/Gala/Gala.swift
+++ Sources/Gala/Gala.swift
@@ -2,7 +2,7 @@ import SwiftUI
 
 
 public func NightAndDay<A: View>(_ name: String? = nil, @ViewBuilder items: @escaping () -> A) -> some View {
-    ForEach([ColorScheme.light, .dark], id: \.self) { scheme in
+    ForEach(ColorScheme.allCases, id: \.self) { scheme in
         items()
             .previewDisplayName(name.map { "\($0) \(scheme)" } ?? "\(scheme)")
             .environment(\.colorScheme, scheme)
@@ -44,7 +44,7 @@ public func VerticalSizeClasses<A: View>(_ name: String? = nil, @ViewBuilder ite
 
 
 public func LayoutDirections<A: View>(_ name: String? = nil, @ViewBuilder items: @escaping () -> A) -> some View {
-    ForEach([LayoutDirection.leftToRight, .rightToLeft], id: \.self) { item in
+    ForEach(LayoutDirection.allCases, id: \.self) { item in
         items()
             .previewDisplayName(name.map { "\($0) \(item)" } ?? "\(item)")
             .environment(\.layoutDirection, item)
